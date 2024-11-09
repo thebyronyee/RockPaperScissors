479f75a364f9166f09c879a01a4deb35912f76c0
thebyronyee <thebyronyee@gmail.com>
2024-11-09 15:08:11 +0800

    Fixing link of js file to html

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
+//Testing if js file linked to html file - console log will display "Hello World!"
+console.log("Hello World!");
+
 //getComputerChoice: 
     //getComputerChoice will randomly RETURN a string of "rock", "paper", or "scissors"
-    //Randome Number: Use Math.random to generate random number 1-3
+    //Random Number: Use Math.random to generate random number 1-3
     //Variable string: Assign random number to rock/paper/scissors
 
 //function getComputerChoice () {
@@ -12,4 +15,3 @@
     let randomNumber = getRandomIntInclusive(1, 3);
     console.log(randomNumber);
 
-    
