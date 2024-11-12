7a12bf32306ea912abf00b6a76b70debfc11c4ea
thebyronyee <thebyronyee@gmail.com>
2024-11-12 16:51:14 +0800

    updates on 11/12

--- code.js
+++ code.js
@@ -9,15 +9,30 @@ console.log("Hello World!");
 
     function getComputerChoice() {
         let randomNumber = Math.floor(Math.random() * 3) + 1;
-        if(randomNumber === 1) {return "Rock"}
-        else if(randomNumber === 2) {return "Paper"}
-        else{return "Scissors"}
+        if(randomNumber === 1) {return "rock"}
+        else if(randomNumber === 2) {return "paper"}
+        else{return "scissors"}
     }
     
 //getHumanChoice: 
-    //Prompt human to choose b/t Rock/Paper/Scissors
-    //Console will return human choice
+    //Prompt human to choose b/t rock/paper/scissors
+    //If typed selection is one of those 3 options, console will return human choice
+    //If not, then prompt user to try again
     
+    
+    function getHumanChoice() {
+        let a = "rock"
+        let b = "paper"
+        let c = "scissors"
+        let selection = prompt("Please type one of the following letter options: 'a = rock', 'b = paper', or 'c = scissors'.")
+        //Problem: logic is not matching string. perhaps try  with a variable?
+        if(selection = "a") {
+            console.log("You chose " + selection)
+        }
+        else{
+            prompt("I'm sorry, I don't recognize that option. Please choose again from the following options by typing: 'rock', 'paper', or 'scissors'.")
+        }
+    }
 
 
     
