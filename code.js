89bf3dda06b058058b43859053cc6d336a595e9a
thebyronyee <thebyronyee@gmail.com>
2024-11-16 15:00:39 +0800

    updates to Human Choice and Computer Choice

--- code.js
+++ code.js
@@ -5,23 +5,35 @@ console.log("Hello World!");
     //getComputerChoice will randomly RETURN a string of "rock", "paper", or "scissors"
     //Random Number: Use Math.random to generate random number 1-3
     //Variable string: Assign random number; 1 = rock, 2 = paper 3 = scissors
-    //Console will return string result
+    //computerChoice: string of the computer choice: rock/paper/scissors
+    //Console will return string result "the computer chose..."
 
     function getComputerChoice() {
+        let computerChoice;
         let randomNumber = Math.floor(Math.random() * 3) + 1;
-        if(randomNumber === 1) {return "rock"}
-        else if(randomNumber === 2) {return "paper"}
-        else{return "scissors"}
+        if(randomNumber === 1) {
+            computerChoice = "rock";
+            return "The computer chose rock"
+            }
+        else if(randomNumber === 2) {
+            computerChoice = "paper";
+            return "The computer chose paper"
+            }
+        else{
+            computerChoice = "scissors";
+            return "The computer chose scissors"
+            }
     }
     
 //getHumanChoice: 
     //Prompt human to choose b/t rock/paper/scissors
     //If typed selection is one of those 3 options, console will return human choice
+    //Make human response case-insensitive
     //If not, then prompt user to try again
     function getHumanChoice() {
-        let text;
         let selection = prompt("Please type one of the following letter options: 'rock', 'paper', or 'scissors'.")
-        switch(selection) {
+        let humanChoice = selection.toLowerCase();
+        switch(humanChoice) {
         case  "rock":
             console.log("You chose rock");
             break;
@@ -32,7 +44,28 @@ console.log("Hello World!");
             console.log("You chose scissors");
             break;
         default: 
-            console.log("I'm sorry, I don't recognize that option. Please choose again from the following options by typing: 'rock', 'paper', or 'scissors'.")
+            console.log("I'm sorry, I don't recognize that option. Please try again.");
         }
+        //how can I loop this back to the original prompt?
     }
 
+//Declaring Score Variables:
+    //humanScore keeps track of human points
+    //computerScore keeps track of computer points
+    //Initialize variables with starting value of 0
+   
+let humanScore = 0
+let computerScore = 0
+
+ //playRound: function that has 2 parameters: humanChoice, computerChoice
+        //Determine who wins each round: paper > rock, rock > scissors, scissors > paper; state winner of round "x beats y, human/computer scores 1 point!"
+        //If human wins, add 1pt to human score
+        //If computer wins, add 1 pt to computer score
+        //Display scoreCard at the end of each round
+function playRound(humanChoice, computerChoice) {
+    
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
