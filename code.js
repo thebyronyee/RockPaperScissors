6065323f00ae007ac80be06958490435a6ade1a1
Byron Yee <thebyronye@gmail.com>
2024-11-19 22:37:00 +0800

    Create function playGame. Something not working with the round# counts. Suspect the ties are messing up the count.

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ let humanChoice;
 let computerChoice;
 let humanScore = 0;
 let computerScore = 0;
-let round = 1;
+let round = 0;
 
 //getComputerChoice: 
     //getComputerChoice will randomly RETURN a string of "rock", "paper", or "scissors"
@@ -44,7 +44,7 @@ let round = 1;
         //If human wins, add 1pt to human score
         //If computer wins, add 1 pt to computer score
         //SCORECARD: Display updated scores at the end of each round
-        
+
 function playRound(humanChoice, computerChoice) {
     let selection;
     let result;
@@ -55,54 +55,63 @@ function playRound(humanChoice, computerChoice) {
         result = "Paper beats rock, computer scored 1 point."
         computerScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if (humanChoice === 'rock' & computerChoice === 'scissors') {
         selection = "You chose " + humanChoice + ". The computer chose " + computerChoice + "."
         result = "Rock beats scissors, you scored 1 point."
         humanScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if (humanChoice === 'rock' & computerChoice === 'rock') {
         result = "Tie, play another round."
         alert(result)
+        console.log(round)
         }
     if (humanChoice === 'paper' & computerChoice === 'scissors') {
         selection = "You chose " + humanChoice + ". The computer chose " + computerChoice + "."
         result = "Scissors beats paper, computer scored 1 point."
         computerScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if (humanChoice === 'paper' & computerChoice === 'rock') {
         selection = "You chose " + humanChoice + ". The computer chose " + computerChoice + "."
         result = "Paper beats rock, you scored 1 point."
         humanScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if (humanChoice === 'paper' & computerChoice === 'paper') {
-        result = "Tie, play another round"
+        result = "Tie, play another round."
         alert(result)
+        console.log(round)
         }
     if (humanChoice === 'scissors' & computerChoice === 'rock') {
         selection = "You chose " + humanChoice + ". The computer chose " + computerChoice + "."
         result = "Rock beats scissors, computer scored 1 point."
         computerScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if(humanChoice === 'scissors' & computerChoice === 'paper') {
         selection = "You chose " + humanChoice + ". The computer chose " + computerChoice + "."
         result = "Scissors beats paper, you scored 1 point."
         humanScore += 1
         scoreCard = "Current Score: Human: " + humanScore + " , Computer: " + computerScore
-        alert(selection + "\n" + result + "\n" + scoreCard)
+        round += 1
+        alert(selection + "\n" + result + "\n" + scoreCard + "\nEnd of Round " + round)
         }
     if (humanChoice === 'scissors' & computerChoice === 'scissors') {
         result = "Tie, play another round."
         alert(result)
+        console.log(round)
         }
 }
 
@@ -111,3 +120,54 @@ const computerSelection = getComputerChoice();
 
 playRound(humanChoice, computerChoice);
 
+//PLAYGAME: function to play 5 rounds (excluding ties)
+    //ROUND: keeps track of # rounds; default starting with 1
+    //IF: round = 1-4, re-run the playRound function.
+    //IF: round = tie, re-run the playRound function; do NOT count as a new round
+    //IF: round = 5; state final score and determine a winner. Resets scores and # rounds to play again.
+function playGame(round) {
+    if(result = "Tie, play another round.") {
+        console.log(round)
+        round -= 1
+        getHumanChoice()
+        getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        console.log(round)
+        }
+    if(round = 1) {
+        getHumanChoice()
+        getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        }
+    if(round = 2) {
+        getHumanChoice()
+        getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        }
+    if(round = 3) {
+        getHumanChoice()
+        getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        }
+    if(round = 4) {
+        getHumanChoice()
+        getComputerChoice()
+        playRound(humanChoice, computerChoice)
+        }
+    if(round = 5) {
+        if(humanScore > computerScore) {
+            alert("Congratulations, you won! Click OK to play again.")
+        }
+        if(humanScore < computerScore) {
+            alert("Sorry, the computer won this time. Click OK to play again.")
+        }
+        //round = 0
+        //humanScore = 0
+        //computerScore = 0
+        //getHumanChoice()
+        //getComputerChoice()
+        //playRound(humanChoice, computerChoice)
+    }
+}
+
+playGame(round);
